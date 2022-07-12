5aaf2a8e47f759b4f41ee718ee8bf93d791f3bc9
AdaoBJr <adaobjr@gmail.com>
2022-07-11 21:06:54 -0400

    refactor: :label: added default variant in ButtonProps on Types

--- src/types/interface/Button/index.ts
+++ src/types/interface/Button/index.ts
@@ -3,7 +3,12 @@ import { ReactFC } from '../React';
 
 export interface ButtonProps extends ReactFC {
   /**
-   *Propriedades CSS do MUI
+   *Propriedades CSS do MUI.
    */
   sx?: SxProps<Theme>;
+  /**
+   * Tipos de variantes de botão.
+   * @default contained
+   */
+  variant?: 'contained' | 'outlined' | 'text';
 }
