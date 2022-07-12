4d0ea8ac0cb56af8092647858ba779296453756d
AdaoBJr <adaobjr@gmail.com>
2022-07-11 21:06:25 -0400

    refactor: :art: added default variant on Button

--- src/lib/ui/Button/Button.tsx
+++ src/lib/ui/Button/Button.tsx
@@ -4,7 +4,7 @@ import { Button as ButtonMUI } from '@mui/material';
 import { ButtonProps } from 'types/interface';
 
 export const Button: React.FC<ButtonProps> = (props) => (
-  <ButtonMUI variant="contained" sx={{ ...props?.sx }}>
+  <ButtonMUI {...props} variant={props.variant || 'contained'}>
     {props.children}
   </ButtonMUI>
 );
